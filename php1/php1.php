c664d8ccd701847777ae8af7635803771496cd6a
Dylan Bouchet <dylan.bouchet@gmail.com>
2017-02-28 17:04:32 +0100

    Initial commit

---
+++ php1/php1.php
@@ -0,0 +1,15 @@
+<?php
+$students = [
+    "Emmanuel"  => 42,
+    "Thierry"   => 51,
+    "Pascal"    => 45,
+    "Eric"      => 48,
+    "Nicolas"   => 19
+];
+
+
+foreach ($students as $prenoms=>$age)
+    echo $prenoms." ". $age." ". "ans".'<br/>';
+
+$moyenne = array_sum($students)/count($students);
+    echo "La moyenne est de ".$moyenne;
